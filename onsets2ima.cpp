f16111d9ab7fb6b7d3f3d31f97402b61586e6811
Peter van Kranenburg <peter.van.kranenburg@meertens.knaw.nl>
2022-10-20 10:50:13 +0200

    Add command line input to onsets2ima

--- onsets2ima.cpp
+++ onsets2ima.cpp
@@ -49,6 +49,18 @@ vector<double> getNormalizedIMA(vector<int> onsets, bool spectral = false) {
 	return normalzied_weights;
 }
 
+void printHelp() {
+        cout << "Usage: onsets2ima <infile> <outfile> <outfile_spect>" << endl;
+        cout << "       onsets2ima -onsets onset1 onset2 onset3 ... onsetn" << endl;
+        cout << "infile contains space separated onsets (int). One series of onsets per line." << endl;
+        cout << "outfile will contain ima weights. One weight for each onset." << endl;
+        cout << "outfile_spect will contain ima spectral weights. One weight for each onset." << endl;
+        cout << "If -onsets is given, a space separated list of onsets follows." << endl;
+        cout << "Two lines of output will be written to stdout," << endl;
+        cout << "first all ima weights, then all spectral weights." << endl;
+        exit(0);
+}
+
 int main(int argc, const char* argv[]) {
 
     //read file with onsets (one series of onsets per line)
@@ -57,57 +69,86 @@ int main(int argc, const char* argv[]) {
     ofstream outfile, outfile_spect;
     string infile_name, outfile_name, outfile_spect_name;
 
-    if (argc != 4 || string(argv[1]) == "-help") {
-        cout << "Usage: onset2ima <infile> <outfile> <outfile_spect>" << endl;
-        cout << "infile contains space separated onsets (int). One series of onsets per line." << endl;
-        cout << "outfile will contain ima weights. One weight for each onset." << endl;
-        cout << "outfile_spect will contain ima spectral weights. One weight for each onset." << endl;
+    bool files = false; //whether input and output is to files
+
+    if(argc==1) {
+        printHelp();
         exit(0);
     }
 
 	infile_name = string(argv[1]);
-	outfile_name = string(argv[2]);
-    outfile_spect_name = string(argv[3]);
+    if (infile_name == "-onsets") {
+        files = false;
+        if (argc < 3) {
+            printHelp();
+            exit(0);
+        }
+    } else {
+        files = true;
+        if (argc != 4) {
+            printHelp();
+            exit(0);
+        }
+        outfile_name = string(argv[2]);
+        outfile_spect_name = string(argv[3]);
+        outfile.open(outfile_name);
+        outfile_spect.open(outfile_spect_name);
+    }
 
-    infile.open(infile_name);
-    outfile.open(outfile_name);
-    outfile_spect.open(outfile_spect_name);
-
-    string line;
     vector<int> onsets;
     vector<double> ima;
     vector<double> ima_spect;
 
-    while (getline(infile, line))
-    {
+    vector<string> lines;
+    if(files) {
+        infile.open(infile_name);
+        string line = "";
+        while (getline(infile, line)) lines.push_back(line);
+        infile.close();
+    } else {
+        string line = "";
+        line = argv[2];
+        for(int i=3; i<argc; i++) {
+            line = line + " " + argv[i];
+        }
+        lines.push_back(line);
+    }
+
+
+    for(int i=0; i<lines.size(); i++) {
         onsets.clear();
         ima.clear();
         ima_spect.clear();
 
         int onset;
-        stringstream l( line );
+        stringstream l( lines[i] );
         while(l >> onset)  //read as long as there is integer
         onsets.push_back(onset);
 
         ima = getNormalizedIMA(onsets, false);
         ima_spect = getNormalizedIMA(onsets, true);
 
+        if(files) cout.rdbuf(outfile.rdbuf()); // Redirect std::cout to the file.
+            
         for (int i=0; i<ima.size(); i++) {
-            outfile << ima[i] << " ";
+            cout << ima[i] << " ";
         }
-        outfile << endl;
+        cout << endl;
+
+        if(files) cout.rdbuf(outfile_spect.rdbuf()); // Redirect std::cout to the file.
 
         for (int i=0; i<ima_spect.size(); i++) {
-            outfile_spect << ima_spect[i] << " ";
+            cout << ima_spect[i] << " ";
         }
-        outfile_spect << endl;
+        cout << endl;
 
     }
 
-    infile.close();
-    outfile.close();
-    outfile_spect.close();
-
+    if(files) {
+        outfile.close();
+        outfile_spect.close();
+        }
+ 
     return 0;
 
 }
